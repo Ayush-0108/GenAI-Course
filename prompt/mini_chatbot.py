07b5255cced453428a7f431d7f33814cfaba7f21
Ayush-0108 <kumaraayush0108@gmail.com>
2026-07-06 17:13:07 +0530

    chatbot

---
+++ prompt/mini_chatbot.py
@@ -0,0 +1,24 @@
+from langchain_core.messages import SystemMessage, HumanMessage, AIMessage
+from langchain_huggingface import ChatHuggingFace, HuggingFaceEndpoint
+from dotenv import load_dotenv
+
+load_dotenv()
+
+llm = HuggingFaceEndpoint(
+    repo_id="Qwen/Qwen2.5-7B-Instruct",
+    task="text-generation"
+)
+
+model = ChatHuggingFace(llm=llm)
+
+chat_history = [SystemMessage(content="you are a helpful agent")]
+
+while True:
+    user_input = input("User: ")
+    if user_input == "exit":
+        break
+    chat_history.append(HumanMessage(content=user_input))
+    result = model.invoke(chat_history)
+    chat_history.append(AIMessage(content=result.content))
+    print("AI: ",result.content)
+print(chat_history)
