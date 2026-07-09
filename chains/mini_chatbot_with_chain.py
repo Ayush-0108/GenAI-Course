bad569b3fb1a6809fb5dda29d70b7c0bab03132d
Ayush-0108 <kumaraayush0108@gmail.com>
2026-07-09 14:16:07 +0530

    simple chain

---
+++ chains/mini_chatbot_with_chain.py
@@ -0,0 +1,20 @@
+from langchain_groq import ChatGroq
+from langchain_core.prompts import PromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+from dotenv import load_dotenv
+
+load_dotenv()
+topic = input("enter input:")
+
+model = ChatGroq(model = "llama-3.3-70b-versatile")
+
+parser = StrOutputParser()
+
+prompt = PromptTemplate(template = "You are a very helpful agent for {topic}",
+                        input_variables = ["topic"])
+
+chain = prompt | model | parser
+
+result = chain.invoke({"topic":topic})
+
+print(result)
