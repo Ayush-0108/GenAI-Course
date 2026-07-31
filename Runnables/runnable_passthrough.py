41e017df64f433d7986fbbcc51b23d00f2fa541c
Ayush-0108 <kumaraayush0108@gmail.com>
2026-07-31 15:02:21 +0530

    runnable passthrough

---
+++ Runnables/runnable_passthrough.py
@@ -0,0 +1,31 @@
+from langchain_core.runnables import RunnablePassthrough, RunnableSequence, RunnableParallel
+from langchain_core.prompts import PromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+from langchain_groq import ChatGroq
+from dotenv import load_dotenv
+load_dotenv()
+model = ChatGroq(model = "llama-3.3-70b-versatile")
+
+prompt1 = PromptTemplate(
+    template="write a joke about {topic}",
+    input_variables=["topic"]
+)
+prompt2 = PromptTemplate(
+    template="Explain the joke {text}",
+    input_variables=["text"]
+)
+
+parser = StrOutputParser()
+
+joke_gen_chain= RunnableSequence(prompt1,model,parser)
+
+parallel_chain = RunnableParallel({
+    "joke":RunnablePassthrough(),
+    "explanation": RunnableSequence(prompt2,model,parser)
+})
+
+final_chain = RunnableSequence(joke_gen_chain,parallel_chain)
+
+print(final_chain.invoke({"topic":"AI"})["joke"])
+print("-"*100)
+print(final_chain.invoke({"topic":"AI"})["explanation"])
